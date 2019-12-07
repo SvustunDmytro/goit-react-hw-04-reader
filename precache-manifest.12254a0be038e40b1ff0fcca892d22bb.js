7a33e3aa4c4cd5ece0beaebe7a8ba6a2d3f51288
SvisT <svisteckij@gmail.com>
2019-12-07 15:58:37 +0200

    Updates

--- precache-manifest.12254a0be038e40b1ff0fcca892d22bb.js
+++ precache-manifest.12254a0be038e40b1ff0fcca892d22bb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0b994db610080afa553d26f37478590",
+    "revision": "287f5e50a76fc8dae108f78e977e4685",
     "url": "/goit-react-hw-04-reader/index.html"
   },
   {
-    "revision": "4e76fb4ce9e191616207",
+    "revision": "01d5c3c6d7a3e6d73b12",
     "url": "/goit-react-hw-04-reader/static/css/main.d896fb5a.chunk.css"
   },
   {
-    "revision": "ec7ca2bfdfa8e7195597",
-    "url": "/goit-react-hw-04-reader/static/js/2.7093050e.chunk.js"
+    "revision": "360a0c56392d3fe73732",
+    "url": "/goit-react-hw-04-reader/static/js/2.05de82a8.chunk.js"
   },
   {
-    "revision": "4e76fb4ce9e191616207",
-    "url": "/goit-react-hw-04-reader/static/js/main.ce1fa3f3.chunk.js"
+    "revision": "01d5c3c6d7a3e6d73b12",
+    "url": "/goit-react-hw-04-reader/static/js/main.0d4e72dc.chunk.js"
   },
   {
     "revision": "c740ec36e87b31379a70",
