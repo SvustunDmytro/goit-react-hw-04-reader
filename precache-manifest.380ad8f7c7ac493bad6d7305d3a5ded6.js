d6bea5e4cdd990587fe3c9991ed7039f67275f0c
SvisT <svisteckij@gmail.com>
2019-12-07 15:53:12 +0200

    Updates

---
+++ precache-manifest.380ad8f7c7ac493bad6d7305d3a5ded6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b994db610080afa553d26f37478590",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "4e76fb4ce9e191616207",
+    "url": "/goit-react-hw-04-reader/static/css/main.d896fb5a.chunk.css"
+  },
+  {
+    "revision": "ec7ca2bfdfa8e7195597",
+    "url": "/goit-react-hw-04-reader/static/js/2.7093050e.chunk.js"
+  },
+  {
+    "revision": "4e76fb4ce9e191616207",
+    "url": "/goit-react-hw-04-reader/static/js/main.ce1fa3f3.chunk.js"
+  },
+  {
+    "revision": "c740ec36e87b31379a70",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.877735af.js"
+  }
+]);
